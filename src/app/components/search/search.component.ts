8bdcab2d180697c9bfd69e1f60491bc0a17159fc
MysticFantasia <quangphongnhien@gmail.com>
2019-04-08 22:39:50 +0700

    search function

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ResourceService, OcInfraModule } from '@diaas/ux-web';
 
 @Component({
   selector: 'app-search',
@@ -6,11 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  
+  private id='';
+  private distributorId='';
+  private quote = 'https://apig.everest.dxchub.com/evdev/omni-new-business-services/omni/service/quotes';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  search(): void {
+
+
+    if (this.id !== '' && this.distributorId !== '') {
+      this.quote += '?quote-identifier=' + this.id + "&quote-distributor-id=" + this.distributorId;
+      console.log(this.quote);
+    }
+    else if (this.id !== '') {
+      this.quote += '?quote-identifier=' + this.id;
+    }
+    else if (this.distributorId !== '') {
+      this.quote += "?quote-distributor-id=" + this.distributorId;
+    }
+    OcInfraModule.AppInjector.get(ResourceService).updateCreateAliasName('quotes',
+      this.quote, true);
+      console.log(this.quote);
+    this.quote = 'https://apig.everest.dxchub.com/evdev/omni-new-business-services/omni/service/quotes';
+  }
+
+  reset(): void {
+    OcInfraModule.AppInjector.get(ResourceService).updateCreateAliasName('quotes',
+    'https://apig.everest.dxchub.com/evdev/omni-new-business-services/omni/service/quotes?', true);
+       console.log(this.quote);
   }
 
 }
